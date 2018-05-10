ee58cd506c5fedcabe880b6f6a6ae36f9b1d2955
Sergey Bendas <seregabendas@gmail.com>
2018-05-10 22:45:10 +0300

    Check is Finger on

--- arduino/sketch/OxyArduino/OxyArduino2.ino/OxyArduino2.ino.ino
+++ arduino/sketch/OxyArduino/OxyArduino2.ino/OxyArduino2.ino.ino
@@ -6,7 +6,8 @@
 #define IR_LED_CURRENT        MAX30100_LED_CURR_20_8MA
 #define RED_LED_CURRENT_ON    MAX30100_LED_CURR_20_8MA
 #define RED_LED_CURRENT_OFF   MAX30100_LED_CURR_0MA
-#define OLED_RESET 4
+#define OLED_RESET            4
+#define SCANNING              "Scanning..."
 Adafruit_SSD1306 display(OLED_RESET);
 MAX30100 sensor;
 boolean isEnable;
@@ -32,24 +33,30 @@ void loop() {
   uint16_t ir, red;
   sensor.update();
   sensor.getRawValues(&ir, &red);
+  isEnable = enableRedLed(ir, isEnable);
+  if (isEnable) {
+    printToMonitorVal(ir, red);
+    printToSerialVal(ir, red);
+  } else {
+    printToMonitorScan();
+  }
+}
+boolean enableRedLed(uint16_t ir, boolean isEnable) {
   if (isEnable && ir < 10000) {
     sensor.setLedsCurrent(IR_LED_CURRENT, RED_LED_CURRENT_OFF);
-    isEnable = false;
+    return false;
   } else if (!isEnable && ir > 10000) {
     sensor.setLedsCurrent(IR_LED_CURRENT, RED_LED_CURRENT_ON);
-    isEnable = true;
+    return true;
   }
-  printToMonitor(ir, red);
-  printToSerial(ir, red);
 }
-
-void printToSerial(uint16_t ir, uint16_t red) {
+void printToSerialVal(uint16_t ir, uint16_t red) {
   Serial.println(ir);
   Serial.println(red);
   Serial.println();
 }
 
-void printToMonitor(uint16_t ir, uint16_t red) {
+void printToMonitorVal(uint16_t ir, uint16_t red) {
   display.clearDisplay();
   display.setTextSize(1);
   display.setTextColor(WHITE);
@@ -59,6 +66,16 @@ void printToMonitor(uint16_t ir, uint16_t red) {
   display.print("Ired:  ");
   display.println(ir);
   display.display();
+}
+
+void printToMonitorScan() {
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 5);
+  display.print("Put finger");
+  display.display();
+  
 }
 
 
