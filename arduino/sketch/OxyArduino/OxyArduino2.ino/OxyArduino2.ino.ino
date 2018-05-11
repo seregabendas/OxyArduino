e3d4a4c88be60ee0bedbea0b559f5ebe0164f1bd
Sergey Bendas <seregabendas@gmail.com>
2018-05-11 21:25:08 +0300

    Avarage ir/red, calculate ShO2, Json to serial

--- arduino/sketch/OxyArduino/OxyArduino2.ino/OxyArduino2.ino.ino
+++ arduino/sketch/OxyArduino/OxyArduino2.ino/OxyArduino2.ino.ino
@@ -1,3 +1,5 @@
+#include <ArduinoJson.h>
+
 
 #include <Adafruit_SSD1306.h>
 #include <Wire.h>
@@ -8,46 +10,101 @@
 #define RED_LED_CURRENT_OFF   MAX30100_LED_CURR_0MA
 #define OLED_RESET            4
 #define SCANNING              "Scanning..."
+
 Adafruit_SSD1306 display(OLED_RESET);
 MAX30100 sensor;
 boolean isEnable;
+
 void setup() {
   Serial.begin(115200);
   Serial.print("Initializing MAX30100..");
 
   if (!sensor.begin()) {
     Serial.println("FAILED");
+    for (;;);
   } else {
     Serial.println("SUCCESS");
   }
   sensor.setMode(MAX30100_MODE_SPO2_HR);
   sensor.setLedsCurrent(IR_LED_CURRENT, RED_LED_CURRENT_OFF);
-  isEnable = false;
   sensor.setHighresModeEnabled(true);
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
   display.clearDisplay();
 }
 
 void loop() {
+  unsigned long start = millis();
+  enableRedLed(checkIrLed());
+  if (checkIrLed()) {
+    int ir, red;
+    while (!getAvarageValues(&ir, &red, 300)) {
+      if (!checkIrLed()) {
+        return;
+      }
+    }
+    int ShO2 = calculateShO2(ir, red);
+    JsonObject& json = getJsonT(ir, red, ShO2);
+    json.printTo(Serial);
+    Serial.println();
+    printToMonitorVal(ir, red);
+  } else {
+    JsonObject& json = getJsonF();
+    json.printTo(Serial);
+    Serial.println();
+    printToMonitorText("Put finger");
+  }
+}
 
+JsonObject& getJsonF() {
+  StaticJsonBuffer<200> jsonBuffer;
+  JsonObject& root = jsonBuffer.createObject();
+  root["device"] = "Tyrol_v2";
+  root["status"] = false;
+  return root;
+}
+
+JsonObject& getJsonT(int ir, int red, int ShO2) {
+  StaticJsonBuffer<200> jsonBuffer;
+  JsonObject& root = jsonBuffer.createObject();
+  JsonObject& values = jsonBuffer.createObject();
+
+  root["device"] = "Tyrol_v2";
+  root["status"] = true;
+  values["ir"] = ir;
+  values["red"] = red;
+  values["ShO2"] = ShO2;
+  root["values"] = values;
+  return root;
+}
+int calculateShO2(long ir, long red) {
+  return (float)red / (ir + red) * 100;
+}
+boolean getAvarageValues(int *irResult, int *redResult, int num) {
+  long irRes = 0;
+  long redRes = 0;
+  for (int i = 0; i < num; i++) {
+    uint16_t ir, red;
+    sensor.update();
+    sensor.getRawValues(&ir, &red);
+
+    irRes = irRes + ir;
+    redRes = redRes + red;
+  }
+  *irResult = irRes / num;
+  *redResult = redRes / num;
+  return true;
+}
+boolean checkIrLed() {
   uint16_t ir, red;
   sensor.update();
   sensor.getRawValues(&ir, &red);
-  isEnable = enableRedLed(ir, isEnable);
-  if (isEnable) {
-    printToMonitorVal(ir, red);
-    printToSerialVal(ir, red);
-  } else {
-    printToMonitorScan();
-  }
+  return ir > 10000;
 }
-boolean enableRedLed(uint16_t ir, boolean isEnable) {
-  if (isEnable && ir < 10000) {
-    sensor.setLedsCurrent(IR_LED_CURRENT, RED_LED_CURRENT_OFF);
-    return false;
-  } else if (!isEnable && ir > 10000) {
+void enableRedLed(boolean isEnable) {
+  if (isEnable) {
     sensor.setLedsCurrent(IR_LED_CURRENT, RED_LED_CURRENT_ON);
-    return true;
+  } else if (!isEnable) {
+    sensor.setLedsCurrent(IR_LED_CURRENT, RED_LED_CURRENT_OFF);
   }
 }
 void printToSerialVal(uint16_t ir, uint16_t red) {
@@ -68,14 +125,14 @@ void printToMonitorVal(uint16_t ir, uint16_t red) {
   display.display();
 }
 
-void printToMonitorScan() {
+void printToMonitorText(String text) {
   display.clearDisplay();
   display.setTextSize(2);
   display.setTextColor(WHITE);
   display.setCursor(0, 5);
-  display.print("Put finger");
+  display.print(text);
   display.display();
-  
+
 }
 
 
